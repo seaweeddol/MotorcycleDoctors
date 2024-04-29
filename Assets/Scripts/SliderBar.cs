36a763d79ca686311927234cc072676a3a6a3c8a
Jessica <jstone4815@gmail.com>
2024-04-29 23:07:44 +0300

    make SliderBar more reusable
    Added label and icon SerializeFields, and added #/# _valueText

--- Assets/Scripts/SliderBar.cs
+++ Assets/Scripts/SliderBar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,22 +8,47 @@ public class SliderBar : MonoBehaviour
 {
     [SerializeField]
     private Slider _slider;
+
     [SerializeField]
     private Gradient _gradient;
+
     [SerializeField]
     private Image _fill;
+
+    [SerializeField]
+    private TextMeshProUGUI _labelTMP;
+
+    [SerializeField]
+    private Image _iconImage;
+
+    [SerializeField]
+    private TextMeshProUGUI _valueText;
+
+    [Header("Provide values here")]
+    [SerializeField]
+    private string _labelText;
+    [SerializeField]
+    private Sprite _iconSprite;
+
+
+    void Start()
+    {
+        _labelTMP.text = _labelText;
+        _iconImage.sprite = _iconSprite;
+    }
 
     public void SetMaxValue(int value)
     {
         _slider.maxValue = value;
         _slider.value = value;
-
         _fill.color = _gradient.Evaluate(1f);
+        _valueText.text = value + "/" + _slider.maxValue;
     }
 
     public void SetCurrentValue(int value)
     {
         _slider.value = value;
         _fill.color = _gradient.Evaluate(_slider.normalizedValue);
+        _valueText.text = value + "/" + _slider.maxValue;
     }
 }
